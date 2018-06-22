7a59ea3f517c7c715f6b20c4f0b360094a11fb0c
Luc Princen <hi@lucp.nl>
2018-06-22 15:55:26 +0200

    First public version

---
+++ README.md
@@ -0,0 +1,108 @@
+# Gutenberg Sortable
+A Gutenberg component to turn anything into an animated, touch- and a11y-friendly sortable list.
+
+---
+
+## Features
+
+- Locked axises
+- Events
+- Smooth animations
+- Auto-scrolling
+- Horizontal, vertical and grid lists
+- Touch support 👌
+- Keyboard support 💪
+
+---
+
+## Installation
+
+Using npm:
+`$ npm install gutenberg-sortable --save`
+
+And then, using a module bundler that supports ES2015 modules (like [webpack](https://webpack.js.org/)):
+```
+import {Sortable} from 'gutenberg-sortable';
+
+//or, if you're not using ES6:
+var Sortable = require('gutenberg-sortable');
+```
+
+---
+
+## Usage
+
+Here's a basic example of the Sortable gutenberg component, used in a block.
+```
+    //... skipping the usual registerBlockType settings, and getting straight to the attributes:
+    attributes: {
+        images: {
+            source: 'query',
+            selector: 'img',
+            query: {
+                url: { source: 'attribute', attribute: 'src' },
+                alt: { source: 'attribute', attribute: 'alt' }
+            }
+        }
+    },
+    edit( props ) {
+
+        let images = ( !props.attributes.images ? [] : props.attributes.images );
+        const { className, setAttributes } = props
+
+        return (
+            <div className={className}>
+                <Sortable
+                    className="gallery"
+                    items={images}
+                    axis="grid"
+                    onSortEnd={ ( images ) => setAttributes({ images }) }
+                >
+                    {images.map((image) =>
+                        <img src={image.url} alt={image.alt} class="gallery-image" />
+                    )}
+                </Sortable>
+            </div>
+        )
+    },
+    //... rest of the block logic
+```
+
+Let's break that down:
+
+### Attributes
+When you register an attribute to work with Sortable, it's probably easiest to use a source: 'query' attribute. This makes it so you can just add html or components to your Sortable.
+
+### Sortable 
+Sortable is meant as a wrapper. Wrap it around everything you'd like to be sortable. It will add a parent div around all the children. You should also pass the attribute (in this case children) as a prop called "items". This ensures you will get back the re-sorted prop in your sortable events.
+
+There's a few options you can pass the component:
+
+*axis* - The axis you'd like to sort on. This example is set to 'grid', but X and Y are also available. Y is the default.
+
+*onSortStart* - What to do when sorting starts. This is a function that will get the node and it's index plus the event as it's properties returned. A simple example for this:
+```
+const highlight = ({node, index}, event) => {
+    node.classList.add('highlight');
+    console.log( 'the element you\'ve picked up has an index of '+index );
+}
+
+<Sortable onSortStart={highlight}>
+```
+This will give the picked up node a "highlight" class and log a message with the nodes current index.
+
+
+*onSortEnd* - What to do when sorting has finished. This function will return the items you passed along as a prop, but now reordered according to the users' action. In the basic example above we just reset the attribute with the new sorted values.
+
+---
+
+## Dependencies
+
+Gutenberg Sortable depends on the [react-sortable-hoc](https://github.com/clauderic/react-sortable-hoc) package by [Claudéric Demers](https://github.com/clauderic).
+
+
+---
+
+## Contributions
+
+All help is welcome! Please live your feature- and/org pull-request here! 😉
---
+++ index.js
@@ -0,0 +1,201 @@
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
+import classnames from 'classnames';
+import React from 'react';
+
+const { Component } = wp.element;
+
+class Sortable extends Component {
+
+    //constructor
+    constructor() {
+        super(...arguments);
+
+        this.focusIndex = null;
+
+        this.onSortStart = this.onSortStart.bind(this);
+        this.onSortEnd = this.onSortEnd.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
+    }
+
+
+    /**
+     * Get the sortable list:
+     */
+    getSortableList() {
+
+        const { items, children, className } = this.props;
+
+        //create the sortable container:
+        return SortableContainer(() => {
+
+            //loop through all available children
+            return (
+                <div className={classnames('components-sortable', className)}>
+                    {children.map((child, index) => {
+
+                        child.props['tabindex'] = '0';
+                        child.props['onKeyDown'] = this.onKeyDown;
+
+                        //generate a SortableElement using the item and the child
+                        let SortableItem = SortableElement(() => {
+                            return (child)
+                        });
+
+                        //set a temporary class so we can find it post-render:
+                        if (index == this.focusIndex) {
+                            child.props['class'] = classnames('sortable-focus', child.props.className);
+                        }
+
+                        //display Sortable Element
+                        return (
+                            <SortableItem key={`item-${index}`} index={index} item={items[index]} />
+                        )
+                    })}
+                </div>
+            )
+        });
+    }
+
+
+    /**
+     * Render the component
+     */
+    render() {
+        const items = this.props.items;
+        const SortableList = this.getSortableList();
+
+        //reset key-focus after refresh:
+        this.resetKeyboardFocus();
+
+        return (
+            //return the sortable list, with props from our upper-lever component
+            <SortableList
+                axis={this.getAxis()}
+                items={items}
+                onSortEnd={this.onSortEnd}
+            />
+        );
+    }
+
+    /*************************************/
+    /**         Evens                    */
+    /*************************************/
+
+    /**
+     * What to do on sort start ?
+     * 
+     * @param Object
+     */
+    onSortStart({ node, index, collection }, event) {
+        //run the corresponding function in the upper-lever component:
+        if (typeof (this.props.onSortStart) === 'function') {
+            this.props.onSortStart({ node, index, collection }, event);
+        }
+    }
+
+    /**
+     * What to do on sort end?
+     * 
+     * @param Object holding old and new indexes and the collection 
+     */
+    onSortEnd({ oldIndex, newIndex }) {
+
+        //create a new items array:
+        let _items = arrayMove(this.props.items, oldIndex, newIndex);
+
+        //and run the corresponding function in the upper-lever component:
+        if (typeof (this.props.onSortEnd) === 'function') {
+            this.props.onSortEnd(_items);
+        }
+    }
+
+
+    /*************************************/
+    /**         Helpers                  */
+    /*************************************/
+
+    /**
+     * Get a default axis, and allow for the "grid" axis type
+     */
+    getAxis() {
+        if (typeof (this.props.axis) == 'undefined') {
+            return 'y';
+        } else if (this.props.axis == 'grid') {
+            return 'xy';
+        }
+
+        return this.props.axis;
+    }
+
+
+
+
+    /*************************************/
+    /**         Keyboard Accesibility    */
+    /*************************************/
+
+    /**
+     * Keyboard accessibility:
+     */
+    onKeyDown(e) {
+
+        if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        const key = e.keyCode;
+        const oldIndex = this.getIndex(e.target);
+        let newIndex = oldIndex;
+
+        switch (key) {
+
+            case 37: //left
+            case 38: //top
+                newIndex = parseInt(oldIndex - 1);
+                break
+            case 39: //right
+            case 40: //down
+                newIndex = parseInt(oldIndex + 1);
+                break;
+        }
+
+        if (oldIndex !== newIndex) {
+            this.focusIndex = newIndex;
+            this.onSortEnd({ oldIndex, newIndex });
+        }
+    }
+
+    /**
+     * Get the index of a child
+     * 
+     * @param Element child 
+     */
+    getIndex(child) {
+        const parent = child.parentNode;
+        const children = parent.children;
+        let i = children.length - 1;
+        for (; i >= 0; i--) {
+            if (child == children[i]) {
+                break;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * After a render, reset the keyboard focus:
+     */
+    resetKeyboardFocus() {
+        setTimeout(() => {
+            if (this.focusIndex !== null) {
+                const focusElement = document.getElementsByClassName('sortable-focus')[0];
+                focusElement.focus();
+                focusElement.classList.remove('sortable-focus');
+                this.focusIndex = null;
+            }
+        }, 10);
+    }
+}
+
+export default Sortable;
